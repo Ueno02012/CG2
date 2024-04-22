a49689480701e89ad3a09349a8f2f43941246eb6
Ueno02012 <k023g0064@g.neec.ac.jp>
2024-04-22 21:54:57 +0900

    test

--- main.cpp
+++ main.cpp
@@ -374,7 +374,32 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
   }
 
 
+  CloseHandle(fenceEvent);
+  fence->Release();
+  rtvDesciptorHeap->Release();
+  swapChainResources[0]->Release();
+  swapChainResources[1]->Release();
+  swapChain->Release();
+  commandList->Release();
+  commandAllocator->Release();
+  commandQueue->Release();
+  device->Release();
+  useAdapter->Release();
+  dxgiFactory->Release();
+#ifdef _DEBUG
+  debugController->Release();
+
+#endif
+
+
+
   //リソースチェック
-  IDXGIDebug1
+  IDXGIDebug1* debug;
+  if (SUCCEEDED(DXGIGetDebugInterface1(0, IID_PPV_ARGS(&debug)))) {
+    debug->ReportLiveObjects(DXGI_DEBUG_ALL, DXGI_DEBUG_RLO_ALL);
+    debug->ReportLiveObjects(DXGI_DEBUG_APP, DXGI_DEBUG_RLO_ALL);
+    debug->ReportLiveObjects(DXGI_DEBUG_D3D12, DXGI_DEBUG_RLO_ALL);
+    debug->Release();
+  }
   return 0;
 }
