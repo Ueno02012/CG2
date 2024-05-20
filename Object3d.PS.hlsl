df12adadc5846a06ef1e3cc067030b4b0604bb8c
Ueno02012 <k023g0064@g.neec.ac.jp>
2024-05-20 17:00:24 +0900

    確認課題

--- Object3d.PS.hlsl
+++ Object3d.PS.hlsl
@@ -1,7 +1,3 @@
-struct PixelShaderOutput
-{
-  float32_t4 color : SV_TARGET0;
-};
 struct Material
 {
   float32_t4 color;
@@ -14,9 +10,10 @@ struct PixelShaderOutput
 };
 
 
+
 PixelShaderOutput main()
 {
   PixelShaderOutput output;
-  output.color = float32_t4(1.0, 1.0, 1.0, 1.0);
+  output.color = gMaterial.color;
 	return output;
 }
