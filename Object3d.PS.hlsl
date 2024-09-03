db54f71714afc9a8493bbfcddfc028fb6b675e3c
Ueno02012 <k023g0064@g.neec.ac.jp>
2024-09-03 12:19:23 +0900

    test

--- Object3d.PS.hlsl
+++ Object3d.PS.hlsl
@@ -1,43 +1,45 @@
-#include "object3d.hlsli"
+#include "Object3d.hlsli"
 
 struct Material
 {
-  float32_t4 color;
-  int32_t enbleLighting;
+    float4 color;
+    int endbleLighting;
 };
-struct PixelShaderOutput
-{
-  float32_t4 color : SV_TARGET0;
-};
+
 struct DirectionalLight
 {
-  float32_t4 color;
-  float32_t3 direction;
-  float intensity;
+    float4 color; //!< ライトの色
+    float3 direction; //!< ライトの向き
+    float intensity; //!< 輝度
 };
 
 ConstantBuffer<Material> gMaterial : register(b0);
 ConstantBuffer<DirectionalLight> gDirectionalLight : register(b1);
 
-Texture2D<float32_t4> gTexture : register(t0);
+struct PixeShaderOutput
+{
+    float4 color : SV_TARGET0;
+    
+};
+
+Texture2D<float4> gTexture : register(t0);
 SamplerState gSampler : register(s0);
 
-PixelShaderOutput main(VertexShaderOutput input)
+PixeShaderOutput main(VertexShaderOutput input)
 {
-  PixelShaderOutput output;
-  
-  float32_t4 textureColor = gTexture.Sample(gSampler, input.texcoord);
-  //output.color = gMaterial.color * textureColor;
-
-  if (gMaterial.enbleLighting != 0)
-  {
-    float cos = saturate(dot(normalize(input.normal), -gDirectionalLight.direction));
-    output.color = gMaterial.color * textureColor * gDirectionalLight.color * cos * gDirectionalLight.intensity;
+    //TextureをSampling
+    float4 textureColor = gTexture.Sample(gSampler, input.texcoord);
     
-  } else {
-    output.color = gMaterial.color * textureColor;
-  }
-
-  
-	return output;
+    PixeShaderOutput output;
+    
+    if (gMaterial.endbleLighting != 0){ // Linhthingする場合
+        // half lambert
+        float Ndont = dot(normalize(input.normal), -gDirectionalLight.direction);
+        float cos = pow(Ndont * 0.5f + 0.5f, 2.0f);
+        output.color = gMaterial.color * textureColor * gDirectionalLight.color * cos * gDirectionalLight.intensity;
+    }
+    else{ // Linhthingしない場合、前回までと同じ演算
+        output.color = gMaterial.color * textureColor;
+    }
+    return output;
 }
